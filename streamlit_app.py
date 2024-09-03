0a82c526ed56a35deed15cca939f03d214c8fd9a
nkomuravelly <84181198+nkomuravelly@users.noreply.github.com>
2024-09-03 22:04:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 cnx = st.connection("snowflake")
 # Write directly to the app
@@ -24,6 +25,9 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 if ingredients_list:
     #st.write(ingredients_list)
